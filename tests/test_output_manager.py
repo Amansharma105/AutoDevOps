cb7e376a4d83ed45dd148fde8507a686bc405541
Amansharma105 <sharmaaman87600@gmail.com>
2026-08-21 22:35:17 +0530

    Add test for OutputManager's target directory

---
+++ tests/test_output_manager.py
@@ -0,0 +1,11 @@
+from generators.output_manager import OutputManager
+
+
+def test_output_manager(tmp_path):
+
+    manager = OutputManager(tmp_path)
+
+    directory = manager.get_target_directory("terraform")
+
+    assert directory.exists()
+    assert directory.name == "terraform"
