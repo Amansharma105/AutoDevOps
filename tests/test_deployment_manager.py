258e79d0cf96d2121819588efe6cb6476f4d9e48
Amansharma105 <sharmaaman87600@gmail.com>
2026-08-22 14:41:58 +0530

    Update test_deployment_manager.py

--- tests/test_deployment_manager.py
+++ tests/test_deployment_manager.py
@@ -7,4 +7,5 @@ def test_deployment_manager():
 
     assert manager is not None
     assert manager.terraform is not None
+    assert manager.ansible is not None
     assert manager.logger is not None
